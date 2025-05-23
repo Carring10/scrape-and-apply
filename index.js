2bdaa85ca184aa2c5844325bbfff90c04c0fb730
Carrington <hartis.c99@gmail.com>
2025-05-23 14:48:58 -0400

    scrape url and role name

--- index.js
+++ index.js
@@ -4,18 +4,34 @@ const axios = require("axios");
 async function performScraping() {
     const axiosResponse = await axios.request({
         method: "GET",
-        url: "https://",
+        url: "https://ally.avature.net/careers/SearchJobs/software%20engineer?667=%5B265477%5D&667_format=613&listFilterMode=1&jobRecordsPerPage=6&",
         headers: {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
         }
-    })
+    });
+
+    const $ = cheerio.load(axiosResponse.data);
+    const filteredRoles = [];
+
+    $(".article.article--result").each((_, element) => {
+        const name = $(element).find("a").first().text().trim();
+        const pageUrl = $(element).find("a").attr("href");
+
+        console.log("Role: ", name, "Listing:", pageUrl);
+
+        if (name && pageUrl) {
+
+            const allyBank = {
+                name: name,
+                url: pageUrl
+            }
+
+            filteredRoles.push(allyBank);
+        }
+    });
+    console.log(filteredRoles);
 }
 
-// parsing the HTML source of the target web page with Cheerio
-const $ = cheerio.load(axiosResponse.data);
-
-// initializing the data structures that will contain the scraped data
-const internships = [];
 const juniorRoles = [];
 
 performScraping();
