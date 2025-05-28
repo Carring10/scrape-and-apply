06d287aa69de9ff249cbf99884e51b3dbf7a4fc2
Carrington <hartis.c99@gmail.com>
2025-05-28 17:30:26 -0400

    add the name of the role and url to obj

--- truist.js
+++ truist.js
@@ -1,20 +1,26 @@
 const puppeteer = require("puppeteer");
 
 const truistScraping = async () => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
 
-  await page.goto("https://careers.truist.com/us/en/search-results?keywords=software%20engineer%20intern&from=20&s=1", { waitUntil: "networkidle2" });
+    await page.goto("https://careers.truist.com/us/en/search-results?keywords=software%20engineer&p=65a8e27d8879283831b664bd8b7f0ad4&location=Charlotte,%20North%20Carolina,%20United%20States", { waitUntil: "networkidle2" });
 
-  await page.waitForSelector(".jobs-list-item");
+    await page.waitForSelector(".jobs-list-item");
 
-  const jobLinks = await page.$$eval(".jobs-list-item a", anchors =>
-    anchors.map(a => a.href)
-  );
+    const truist = [];
 
-  console.log("Job URLs:", jobLinks);
+    const jobLinks = await page.$$eval(".jobs-list-item a", anchors =>
+        anchors.map(a => ({
+            name: a.textContent.trim(),
+            url: a.href
+        }))
+    );
 
-  await browser.close();
+    truist.push(jobLinks);
+    console.log(truist);
+
+    await browser.close();
 };
 
 truistScraping();
