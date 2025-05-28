fbc14bbff4efae643ddb8105d89a824a21e99eea
Carrington <hartis.c99@gmail.com>
2025-05-28 17:50:22 -0400

    wells fargo scraping

---
+++ wellsFargo.js
@@ -0,0 +1,32 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+async function wellsFargoScraping() {
+    const axiosResponse = await axios.request({
+        method: "GET",
+        url: "https://www.wellsfargojobs.com/en/jobs/?search=software+engineer+intern&location=CHARLOTTE&pagesize=20#results",
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+        }
+    });
+
+    const $ = cheerio.load(axiosResponse.data);
+    const wellsFargo = [];
+
+    $(".card.card-job").each((_, element) => {
+        const name = $(element).find("a").first().text().trim();
+        const pageUrl = $(element).find("a").attr("href");
+
+        if (name && pageUrl) {
+             const filteredRoles = {
+                name: name,
+                url: pageUrl
+            }
+
+            wellsFargo.push(filteredRoles);
+        }
+    });
+    console.log(wellsFargo);
+}
+
+wellsFargoScraping();
