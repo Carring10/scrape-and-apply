031b7b89ad7202cbf19fd46b1e15c87e9c4ab8f8
Carrington <hartis.c99@gmail.com>
2025-05-31 14:37:15 -0400

    connected to mysql database

---
+++ connection.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+// connect to the MySQL database
+connection.connect((error) => {
+    if (error) {
+        console.error('Error connecting to MySQL database:', error);
+    } else {
+        console.log('Connected to MySQL database!');
+    }
+});
+
+connection.end();
